70fc7d21850d940c9f7bc76425badf5f9c727d64
don1ck <don1ck@narod.ru>
2013-03-29 11:51:35 +0300

    forgotten commit

---
+++ oDeskTimer/Classes/ODTimerWindowManager/ODTimerViewController/ODTimerViewController.m
@@ -0,0 +1,27 @@
+//
+//  ODTimerViewController.m
+//  oDeskTimer
+//
+//  Created by Viktor on 09.11.12.
+//  Copyright (c) 2012 Rus Wizards. All rights reserved.
+//
+
+#import "ODTimerViewController.h"
+
+@interface ODTimerViewController ()
+
+@end
+
+@implementation ODTimerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
